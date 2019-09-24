8ec968966187ba33e979e3a97e48e25448b32101
yiminghe <yiminghe@gmail.com>
2019-09-24 14:28:09 +0800

    seperate components

--- components/counter-viewer/counter-viewer.js
+++ components/counter-viewer/counter-viewer.js
@@ -2,21 +2,10 @@ import connectComponent from "../../connect/connectComponent";
 import { CANCEL_COUNTER, INCREMENT_COUNTER_ASYNC } from "../../store/index/ACTION";
 
 Component(connectComponent({
-  methods: {
-    increment() {
-      this.$page.store.dispatch({
-        type: INCREMENT_COUNTER_ASYNC,
-      });
-    },
-    cancel() {
-      this.$page.store.dispatch({
-        type: CANCEL_COUNTER,
-      });
-    },
-  }
 }, {
   mapPageStore(state) {
     return {
+      updating: !!state.counter.updating,
       count: state.counter.count,
     };
   }
